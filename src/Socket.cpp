bb7d8ff6a3326383cd28168c598429debe45b756
paulzhironkin <paulzhironkin@gmail.com>
2023-08-02 15:16:51 +0200

    Realised Socket class with setting up socket

---
+++ src/Socket.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Socket.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:59:15 by latahbah          #+#    #+#             */
+/*   Updated: 2023/08/02 15:15:59 by latahbah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Socket.hpp"
+
+Socket::Socket()
+{
+	//Set up hints to find all addresses to add to socket
+	struct addrinfo hints;
+    memset(&hints, 0, sizeof (struct addrinfo));
+    hints.ai_family = AF_UNSPEC;    /* allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM;/* Stream socket (TCP) */
+    hints.ai_flags = AI_PASSIVE;    /* for wildcard IP address */
+	struct addrinfo *result;		/* starting rointer for getaddrinfo return structsv*/
+	int s;
+	if ((s = getaddrinfo (NULL, PORT, &hints, &result)) != 0) {
+        perror("Getaddrinfo error\n");
+        exit (EXIT_FAILURE);
+    }
+	//socklen_t length;
+    struct addrinfo *rptr; //cycle iterator in addrinfo structs
+	for (rptr = result; rptr != NULL; rptr = rptr -> ai_next) {
+        listener = socket (rptr -> ai_family, rptr -> ai_socktype,
+                       rptr -> ai_protocol);
+        if (listener == -1)
+            continue;
+
+        if (bind(listener, rptr -> ai_addr, rptr -> ai_addrlen) == 0)  // Success
+            break;
+
+        if (close(listener) == -1) {
+			perror("Close error\n");
+			exit(EXIT_FAILURE);
+		}
+    }
+	if (rptr == NULL) {               // Not successful with any address
+        perror("Bind error\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo (result);
+	//Activate socket for listening
+	if (listen (listener, BACKLOG) == -1) {
+		perror("Listen error\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int Socket::get_listener()
+{
+	return listener;
+}
---
+++ src/Socket.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Socket.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:31:45 by latahbah          #+#    #+#             */
+/*   Updated: 2023/08/02 15:16:01 by latahbah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT		"4242"	//port to connect. It's extracted from serv config
+#define BACKLOG		10 		// Max num of connections on socket
+
+class Socket
+{
+private:
+	int listener;			//fd for socket
+public:
+	Socket();
+	int get_listener();
+};
+
+#endif
